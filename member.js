8f90e8ff36db3c910a15fda70dfda85ddfc0a530
Ryan Christman <8922086+rchristman89@users.noreply.github.com>
2023-08-09 15:07:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,38 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "value": 80
+        },
+        {
+            "name": "CSS",
+            "value": 70
+        },
+        {
+            "name": "JavaScript",
+            "value": 60
+        },
+        {
+            "name": "PHP",
+            "value": 50
+        },
+        {
+            "name": "MySQL",
+            "value": 40
+        },
+        {
+            "name": "C",
+            "value": 30
+        },
+        {
+            "name": "C++",
+            "value": 20
+        },
+        {
+            "name": "Java",
+            "value": 10
+        }
+    ];
+
+    return skills;
+}
